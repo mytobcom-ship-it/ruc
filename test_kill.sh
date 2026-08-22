1cf4ba542935377eadd8c405af5f6533815b797b
최정우 <mytobcom@gmail.com>
2026-08-23 07:42:47 +0900

    test 이름 파일 원격 추적 제외

--- test_kill.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# ./test_svr.sh stop 과 동일 (하위 호환)
-exec "$(cd "$(dirname "$0")" && pwd)/test_svr.sh" stop "$@"
--- test_ps.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# ./test_svr.sh ps 와 동일 (하위 호환)
-exec "$(cd "$(dirname "$0")" && pwd)/test_svr.sh" ps "$@"
